ddb44f4cc0f4151fa92e87c1a9ef29f7cf0a8cd7
imabusyman <imabusyman@users.noreply.github.com>
2026-05-25 19:07:21 +0000

    chore: remove dev scratch_refs.cs artifact

--- scratch_refs.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-var asm = AssemblyName.GetAssemblyName("E:/src/CodebrewRouter/Blaze.LlmGateway.LocalInference/bin/Debug/net10.0/Blaze.LlmGateway.LocalInference.dll");
-Console.WriteLine(asm.FullName);
-var refs = Assembly.ReflectionOnlyLoadFrom(asm.CodeBase);
-foreach (var r in refs.GetReferencedAssemblies())
-    Console.WriteLine(r.FullName);
